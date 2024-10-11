b3de78ff1bdec3e55ec5cf0e7df69d7945a580a3
KrithikD <95255679+KrithikDhandapani@users.noreply.github.com>
2024-10-11 16:52:24 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,17 @@ app.append(header);
 
 // Button
 const button = document.createElement("button");
-
-// Fun Emoji
-button.innerHTML = "Launch 🚀";
-
+button.innerHTML = "Launch 🚀"; // Fun emoji 
 app.append(button);
+
+// div element that displays counter
+const counterDisplay = document.createElement("div");
+let counter: number = 0;
+counterDisplay.innerHTML = `${counter} launches 🚀`; // Display initial counter 
+app.append(counterDisplay);
+
+// event listener to increase the counter
+button.addEventListener("click", () => {
+  counter++;
+  counterDisplay.innerHTML = `${counter} launches 🚀`; // Update counter value
+});
