b23f4704d7e5ef00b7f50aea959a8baea0ac9dc7
KrithikD <95255679+KrithikDhandapani@users.noreply.github.com>
2024-10-11 16:42:53 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Button
+const button = document.createElement("button");
+
+// Fun Emoji
+button.innerHTML = "Launch 🚀";
+
+app.append(button);
